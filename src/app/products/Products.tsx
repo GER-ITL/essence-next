8c9a65ed4ee34b81133da274718a27020cc9bea2
GER-ITL <danteryzvelt@inbox.ru>
2024-05-19 18:21:51 +0300

    update

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+import nextSVG from '../../../public/next.svg'
 import styles from './Products.module.scss'
-
 const Products = () => {
-	return <div className={styles.products}>Products</div>
+	return (
+		<div className={styles.products}>
+			<Image className={styles.nextSVG} src={nextSVG} alt='nextSVG' />
+		</div>
+	)
 }
 
 export default Products
